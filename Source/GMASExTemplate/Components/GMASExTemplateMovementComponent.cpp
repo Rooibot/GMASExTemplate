d891bcc6779a058b25488d57243df67c550eddde
Rachel Blackman <packetdancer@gmail.com>
2024-04-17 18:44:27 -0700

    First real revision.

--- Source/GMASExTemplate/Components/GMASExTemplateMovementComponent.cpp
+++ Source/GMASExTemplate/Components/GMASExTemplateMovementComponent.cpp
@@ -20,28 +20,28 @@ void UGMASExTemplateMovementComponent::BindReplicationData_Implementation()
 void UGMASExTemplateMovementComponent::GenAncillaryTick_Implementation(float DeltaTime, bool bLocalMove,
 	bool bCombinedClientMove)
 {
-	if (AbilitySystemComponent) AbilitySystemComponent->GenAncillaryTick(DeltaTime, bCombinedClientMove);
-	
 	Super::GenAncillaryTick_Implementation(DeltaTime, bLocalMove, bCombinedClientMove);
+
+	if (AbilitySystemComponent) AbilitySystemComponent->GenAncillaryTick(DeltaTime, bCombinedClientMove);
 }
 
 void UGMASExTemplateMovementComponent::GenPredictionTick_Implementation(float DeltaTime)
 {
-	if (AbilitySystemComponent) AbilitySystemComponent->GenPredictionTick(DeltaTime);
-	
 	Super::GenPredictionTick_Implementation(DeltaTime);
+
+	if (AbilitySystemComponent) AbilitySystemComponent->GenPredictionTick(DeltaTime);
 }
 
 void UGMASExTemplateMovementComponent::GenSimulationTick_Implementation(float DeltaTime)
 {
-	if (AbilitySystemComponent) AbilitySystemComponent->GenSimulationTick(DeltaTime);
-	
 	Super::GenSimulationTick_Implementation(DeltaTime);
+
+	if (AbilitySystemComponent) AbilitySystemComponent->GenSimulationTick(DeltaTime);
 }
 
 void UGMASExTemplateMovementComponent::PreLocalMoveExecution_Implementation(const FGMC_Move& LocalMove)
 {
-	if (AbilitySystemComponent) AbilitySystemComponent->PreLocalMoveExecution();
-	
 	Super::PreLocalMoveExecution_Implementation(LocalMove);
+
+	if (AbilitySystemComponent) AbilitySystemComponent->PreLocalMoveExecution();
 }
